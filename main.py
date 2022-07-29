e799f5bb2340204937ec2394e829cae7a1790c2c
BarabaSkin <kolyan777.k9@gmail.com>
2022-07-29 16:35:07 +0300

    send user name and last name but if last name doesn't exist print only name

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ bot = telebot.TeleBot('5468350236:AAFvmQA5hlbmRnp9ojVgWjUx7oBKxgCO_dU')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '<b>Привет</b>', parse_mode='html')
+    mess = f'Привет, {message.from_user.first_name} {message.from_user.last_name}'
+    if message.from_user.last_name is None:
+        mess = mess = f'Привет, {message.from_user.first_name}'
+    bot.send_message(message.chat.id, mess)
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
     bot.send_message(message.chat.id, 'Сам справишься')
+
 
 bot.polling(none_stop=True)
