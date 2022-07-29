4b944ae12a8ce404612046a7b4bb3cb9ec88f48c
BarabaSkin <kolyan777.k9@gmail.com>
2022-07-29 16:26:24 +0300

    2 functions: start and help which send messages to user

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("This is my first telegramm bot")
+import telebot
+
+bot = telebot.TeleBot('5468350236:AAFvmQA5hlbmRnp9ojVgWjUx7oBKxgCO_dU')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '<b>Привет</b>', parse_mode='html')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Сам справишься')
+
+bot.polling(none_stop=True)
