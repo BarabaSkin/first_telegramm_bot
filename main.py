34e4c5a4f1fed9b5f2bd4c8885fbb831ad3ece68
BarabaSkin <kolyan777.k9@gmail.com>
2022-07-29 17:05:26 +0300

    add photo to files if user write "photo" bot will senf photo from file

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import telebot
 bot = telebot.TeleBot('5468350236:AAFvmQA5hlbmRnp9ojVgWjUx7oBKxgCO_dU')
 
 
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f'Привет, {message.from_user.first_name} {message.from_user.last_name}'
@@ -17,12 +21,11 @@ def get_user_text(message):
         bot.send_message(message.chat.id, "И тебе привет", parse_mode='html')
     elif message.text == 'id':
         bot.send_message(message.chat.id, f"Твой ID: {message.from_user.id}", parse_mode='html')
+    elif message.text == 'photo':
+        photo = open('4848.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     else:
         echo_all(message)
-
-
-def echo_all(message):
-    bot.reply_to(message, message.text)
 
 
 bot.polling(none_stop=True)
